99a6eeeec861c41ffa7d2b389d4f5006457e0bb2
Abdelqaddous Ali mouch <aali-mou@e1r9p2.1337.ma>
2022-07-18 19:08:21 +0100

    ok

--- Vector.hpp
+++ Vector.hpp
@@ -35,12 +35,91 @@ class Vector
 		typedef typename Allocator::const_pointer		const_pointer;
 		typedef typename std::size_t 						size_type;
 
-		size_type size() const;
-		size_type max_size() const;
-		void resize(size_type sz, T c = T());
-		size_type capacity() const;
-		bool empty()const;
-		void reserve(size_type);
+		size_type size() const
+		{
+			return _size;
+		}
+		size_type max_size() const
+		{
+			//return (2^(64-size(T))-1);
+			return x.max_size();
+		}
+	//	void resize(size_type sz, T c = T());
+		void reserve (size_type n)
+		{
+			if (n > _capacity)
+			{
+				pointer op;
+				op = x.allocate(n);
+				for (int i = 0; i < _size; i++)
+				{
+					x.construct(op + i, arr[i]);
+					x.destroy(arr + i);
+				}
+				x.deallocate(arr, _size);
+				_capacity = n;
+				arr = op;
+			}
+		}
+		iterator begin () const {
+			////
+			return iter;
+		}
+		
+		size_type capacity() const{
+			return _capacity;
+		}
+		bool empty()const
+		{
+			if (_size == 0)
+				return true;
+			return false;
+		}
+		void resize(size_type n,value_type val = value_type() )
+		{
+			if (n < _size)
+			{
+				for (int i = _size; i > n; i--)
+				{
+					x.destroy(arr + i);
+				}
+			}
+			if (n > _size)
+			{
+				if (n  > _capacity)
+				{
+					pointer op;
+					op = x.allocate(n);
+					for (int i = 0; i < _size; i++)
+					{
+						x.construct(op + i, arr[i]);
+						x.destroy(arr + i);
+					}
+					for (int i = _size; i < n; i++)
+					{
+						x.construct(op + i, val);
+					}
+					x.deallocate(arr, _size);
+					_size = n;
+					_capacity = n;
+					arr = op;
+				}
+				else
+				{
+					for (int i = _size; i < n; i++)
+					{
+						x.construct(arr + i, val);
+					}
+				}
+			}
+		}
+
+		void pop_back()
+		{
+			x.destroy(arr + (_size - 1));
+			//if (size)
+			_size = _size - 1;
+		}
 		
 		explicit Vector (const allocator_type& alloc = allocator_type())
 		{
@@ -49,20 +128,21 @@ class Vector
 			_capacity = 0;
 			x = alloc;
 		}
-		//}
-		explicit Vector(size_type n, const T& value = T(), const Allocator& = Allocator())
+
+		explicit Vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 		{
 			arr = x.allocate(n);
+			_size = n;
 			for (size_type i = 0; i < n; i++)
 			{
-				x.construct(this->arr + i, value);
+				x.construct(this->arr + i, val);
 			}
-			std::cout << "DOOOONE_00" << std::endl;
-			for (size_type i = 0; i < n; i++)
-			{
-				std::cout << arr[i] << std::endl;
-			}
-			std::cout << "DOOOONE_01" << std::endl;
+		//	std::cout << "DOOOONE_00" << std::endl;
+		//	for (size_type i = 0; i < n; i++)
+		//	{
+		//		std::cout << arr[i] << std::endl;
+		//	}
+		//	std::cout << "DOOOONE_01" << std::endl;
 			x = alloc;
 		}
 		Vector(const Vector<T,Allocator>& x);
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "Vector.hpp"
-
+#include <vector>
 
 int main()
 {
-    ft::Vector<std::string> a(5, "WEWEWEWEWE");
+   // ft::Vector<std::string> a(5, "WEWEWEWEWE");
+    std::vector<std::string> a(5,"WEWEWEWEWE");
+    a.pop_back();
+    std::cout<<a.max_size()<<std::endl;
     std::cout<<"asdasdasdas"<<std::endl;
 }
